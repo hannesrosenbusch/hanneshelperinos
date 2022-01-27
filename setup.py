fe09742544884871d6413ce417858c99c57d9323
hannesrosenbusch <hannes.rosenbusch@gmail.com>
2022-01-27 14:16:49 +0100

    restructure

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='hanneshelpers',
     url='https://github.com/hannesrosenbusch/hanneshelpers',
     author='Hannes Rosenbusch',
     author_email='',
-    packages=['hanneshelpers'],
+    package_dir = {
+    'surveyexport': 'surveyexport',
+    'sentiment': 'sentiment'},
+    packages=find_packages(),
     install_requires=['easynmt==2.0.1', 'python-docx'],
     version='0.1',
     license='MIT',
