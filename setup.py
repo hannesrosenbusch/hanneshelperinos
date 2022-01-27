d3a523869c90e76e6b5ae75187d2edb7f0238aa9
hannesrosenbusch <hannes.rosenbusch@gmail.com>
2022-01-27 14:57:57 +0100

    restructure

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='hanneshelpers',
     url='https://github.com/hannesrosenbusch/hanneshelpers',
     author='Hannes Rosenbusch',
     author_email='',
-    packages=find_packages('hanneshelpers'),
+    packages=['hanneshelpers'],
     install_requires=['easynmt==2.0.1', 'python-docx'],
     version='0.1',
     license='MIT',
