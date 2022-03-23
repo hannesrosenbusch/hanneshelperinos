d700bd56a239dc5139a8ac0d271533f701f7b6c1
hannesrosenbusch <hannes.rosenbusch@gmail.com>
2022-03-23 11:33:49 +0100

    skip hidden questions

--- hanneshelpers/surveyexport.py
+++ hanneshelpers/surveyexport.py
@@ -106,6 +106,7 @@ def go(inputs):
 
   for i in range(len(survey_raw['questions'])): #question number
       print('haa')
+      print(survey_raw['questions'][i]['hideForCompany'])
       if survey_raw['questions'][i]['hideForCompany'] == 'true':
           print('hehe')
           continue
