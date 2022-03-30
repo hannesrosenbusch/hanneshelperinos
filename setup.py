5c606ac5b64949b10601751605bd439c88b0e448
hannesrosenbusch <hannes.rosenbusch@gmail.com>
2022-03-30 12:07:52 +0200

    stop loading easynmt

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Hannes Rosenbusch',
     author_email='',
     packages=['hanneshelpers'],
-    install_requires=['easynmt==2.0.1', 'python-docx'],
+    install_requires=['python-docx'], #'easynmt==2.0.1', 
     version='0.1',
     license='MIT',
     description='bunch of my functions',
